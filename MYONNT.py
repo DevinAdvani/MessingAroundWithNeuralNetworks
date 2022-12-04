0af4b8872c911bc228ef750aff637e38774a7cdb
ColderThanSeptember <80278580+DevinAdvani@users.noreply.github.com>
2022-12-04 15:49:56 +0000

    Main

--- MYONNT.py
+++ MYONNT.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import scipy.special as sp
+import matplotlib.pyplot as plt
 
 # neural network class definition
 class neuralNetwork: 
@@ -72,9 +73,9 @@ class neuralNetwork:
         return final_outputs
     
 # number of input, hidden and output nodes
-input_nodes = 3
-hidden_nodes = 3
-output_nodes = 3
+input_nodes = 784
+hidden_nodes = 100
+output_nodes = 10
 
 # learning rate is 0.3
 learning_rate = 0.3
@@ -82,3 +83,61 @@ learning_rate = 0.3
 #create instance of neural network
 n = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
 
+# load the mnist training data CSV file into a list
+training_data_file = open("mnist_train_100.csv","r")
+training_data_list = training_data_file.readlines() 
+training_data_file.close()
+
+# load the mnist test data CSV file into a list
+test_data_file = open("mnist_test_10.csv","r")
+test_data_list = test_data_file.readlines()
+test_data_file.close()
+
+# train the neural network
+
+#go through all records in the training data set
+for record in training_data_list:
+    # split the record by the ',' commas
+    all_values = record.split(',')
+    # scale and shift the inputs
+    inputs = (np.asfarray(all_values[1:])/255.0 * 0.99) + 0.01
+    # create the target output values (all 0.01, except the desired label which is 0.99)
+    targets = np.zeros(output_nodes) + 0.01
+    # all_values[0] is the target label for this record
+    targets[int(all_values[0])] = 0.99
+    n.train(inputs,targets)
+    pass
+
+
+# test the neural network
+
+# scorecard for how well the network performs, initially empty
+
+scorecard = []
+
+# go through all the records in the test data set
+for record in test_data_list:
+    # split the record by the "," commas
+    all_values = record.split(",")
+    # correct answer is first value 
+    correct_label = int(all_values[0])
+    print(correct_label, "correct label")
+    # scale and shift the inputs
+    inputs = (np.asfarray(all_values[1:])/255.0 * 0.99) + 0.01
+    # query the network
+    outputs = n.query(inputs)
+    # the index of the highest value corresponds to the label
+    label = np.argmax(outputs)
+    print(label,"network's answer")
+    # append correct or incorrect to list
+    if (label==correct_label):
+        # network's answer matches correct answer, add 1 to scorecard
+        scorecard.append(1)
+    else:
+        # network's answer doesn't match correct answer, add 0 to scorecard
+        scorecard.append(0)
+        pass 
+    
+    pass 
+
+print(scorecard)
